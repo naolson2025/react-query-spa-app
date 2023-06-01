8c3d81b94362101da27e1c26eca0b196405d27a9
Nick Olson <naolson.1819@gmail.com>
2023-06-01 15:36:32 -0500

    initial commit

---
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+
+import type { Treatment } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
+
+// for when we need a query function for useQuery
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
+
+export function useTreatments(): Treatment[] {
+  // since data is undefined on first render, we need to set a fallback
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  return data;
+}
