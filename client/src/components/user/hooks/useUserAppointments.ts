2d04a803856b35ea8e03bedfafdaf83630cd5aac
Nick Olson <naolson.1819@gmail.com>
2023-06-08 07:43:06 -0500

    added auth logic

--- client/src/components/user/hooks/useUserAppointments.ts
+++ client/src/components/user/hooks/useUserAppointments.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { useQuery } from 'react-query';
 
 import type { Appointment, User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -6,17 +7,30 @@ import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
 
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   user: User | null,
-// ): Promise<Appointment[] | null> {
-//   if (!user) return null;
-//   const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
-//     headers: getJWTHeader(user),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  user: User | null,
+): Promise<Appointment[] | null> {
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
+    headers: getJWTHeader(user),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
-  // TODO replace with React Query
-  return [];
+  const { user } = useUser();
+
+  const fallback: Appointment[] = [];
+  const { data: userAppointments = fallback } = useQuery(
+    'userAppointments',
+    () => getUserAppointments(user),
+    {
+      // using the enabled keyword we are making this query dependent on the
+      // user being logged in. We don't want to run this query if the user is
+      // not logged in.
+      enabled: !!user,
+    },
+  );
+
+  return userAppointments;
 }
